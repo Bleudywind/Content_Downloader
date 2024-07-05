e40dc1fde511b96b7fd79139e564d7fb691ea88c
Bleudywind <thom.ferrier@gmail.com>
2024-07-06 01:28:49 +0200

    fix rm-rf

---
+++ init.bat
@@ -0,0 +1,5 @@
+docker build -t yt-converter src
+mkdir src/Musiques
+mkdir src/Video
+mkdir target/Musiques
+mkdir target/Video
--- test.bat
+++
@@ -1,7 +0,0 @@
-
-cd /d src/Musiques
-for /F "delims=" %%i in ('dir /b') do (rmdir "%%i" /s/q || del "%%i" /s/q)
-cd ..
-cd ..
-echo ""
-echo "Done everything as been downloaded !"
